e695c594b4c60187f7a637c6ac6b5fd3ae9a62e2
Fabio Magarelli <42029814+fabiom91@users.noreply.github.com>
2022-08-30 13:31:39 +0100

    fix bat

--- run_windows.bat
+++ run_windows.bat
@@ -1,8 +1,8 @@
-set VIRTUAL_ENV="mat_visualizer_env"
+set "VIRTUAL_ENV=mat_visualizer_env"
 
 :read_mat
 %VIRTUAL_ENV%\Scripts\activate.bat
-pip install -r app_files/requirements.txt
+pip install -r app_files\requirements.txt
 python -c "import sys; sys.path.insert(1,'app_files'); from main import visualize_mat_eeg; visualize_mat_eeg('%1')"
 pause
 EXIT /B 0
@@ -12,4 +12,5 @@ IF EXIST "%VIRTUAL_ENV%\Scripts\activate.bat" (
 ) ELSE (
     pip install virtualenv
     python -m venv mat_visualizer_env
+    CALL :read_mat
 )
